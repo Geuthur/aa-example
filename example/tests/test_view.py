dbc32d3cb139f2d592529bfd41dfca2aeee3e4dc
Geuthur <geuthur@users.noreply.github.com>
2026-01-11 12:41:02 +0100

    [FIX] Test

--- example/tests/test_view.py
+++ example/tests/test_view.py
@@ -4,24 +4,18 @@
 from http import HTTPStatus
 
 # Django
-from django.test import RequestFactory, TestCase
 from django.urls import reverse
 
 # AA Example
+from example.tests import ExampleTestCase
 from example.tests.testdata.utils import create_user_from_evecharacter
 from example.views import index
 
 
-class TestViews(TestCase):
+class TestViews(ExampleTestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.factory = RequestFactory()
-        cls.user = create_user_from_evecharacter(
-            permissions=[
-                "example.basic_access",
-            ]
-        )
 
     def test_view(self):
         request = self.factory.get(reverse("example:index"))
